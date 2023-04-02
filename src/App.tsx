32bbee758c41f042acb5ad79492fd761eda5e431
Szymon KD <104296958+SzymonKDA@users.noreply.github.com>
2023-04-02 10:53:52 +0200

    created basic routes plan in router, also planned directories

---
+++ src/App.tsx
@@ -0,0 +1,118 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+} from "react-router-dom";
+import './App.css';
+import Layout from "./components/Layout";
+// import Checkout from "./style/pages/Checkout";
+import Home from "./style/pages/Home";
+// import ShoppingCart from "./style/pages/ShoppingCart";
+import NotFound from "./style/pages/NotFound";
+//   Link
+
+
+
+
+function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Layout />} >
+      <Route index element={<Home />}></Route>
+      {/* <Route path="/store" element={<StoreLayout />}> */}
+        {/* <Route index element={<Store />}></Route>
+        <Route path="./" element={<StoreAside></StoreAside>}></Route> */}
+      {/* </Route> */}
+      {/* Gotta use protected routes for account routes, remember about that. */}
+      {/* <Route path="/account" element={<Login />}></Route> */}
+      {/* <Route path="/product" element={<ProductLayout />}>
+        <Route path=":productName" element={<Product />}></Route>
+      </Route> */}
+      {/* <Route path="/about" element={<About />}></Route> */}
+      {/* <Route path="/shopping_cart" element={<ShoppingCart />} />
+      <Route path="/checkout" element={<Checkout />} /> */}
+      <Route path="*" element={<NotFound />} />
+    </Route>
+
+  ))
+  return (
+     <RouterProvider router={router} />
+  )
+}
+
+export default App
+
+
+
+
+
+
+// import HostVans, { loader as hostVansLoader} from "./pages/Host/HostVans"
+// import HostVanDetail, { loader as hostVanDetailLoader } from "./pages/Host/HostVanDetail"
+
+// import { requireAuth } from "./utils"
+
+// const router = createBrowserRouter(createRoutesFromElements(
+//   <Route path="/" element={<Layout />}>
+//     <Route index element={<Home />} />
+//     <Route path="about" element={<About />} />
+//     <Route
+//       path="login"
+//       element={<Login />}
+//       loader={loginLoader}
+//       action={loginAction}
+//     />
+//     <Route
+//       path="vans"
+//       element={<Vans />}
+//       errorElement={<Error />}
+//       loader={vansLoader}
+//     />
+//     <Route 
+//       path="vans/:id" 
+//       element={<VanDetail />} 
+//       errorElement={<Error />}
+//       loader={vanDetailLoader}
+//     />
+
+//     <Route path="host" element={<HostLayout />}>
+//       <Route
+//         index
+//         element={<Dashboard />}
+//         loader={dashboardLoader}
+//       />
+//       <Route
+//         path="income"
+//         element={<Income />}
+//         loader={async ({ request }) => await requireAuth(request)}
+//       />
+//       <Route
+//         path="reviews"
+//         element={<Reviews />}
+//         loader={async ({ request }) => await requireAuth(request)}
+//       />
+//       <Route
+//         path="vans"
+//         element={<HostVans />}
+//         errorElement={<Error />}
+//         loader={hostVansLoader}
+//       />
+//       <Route
+//         path="vans/:id"
+//         element={<HostVanDetail />}
+//         errorElement={<Error />}
+//         loader={hostVanDetailLoader}
+//       >
+//         <Route
+//           path="pricing"
+//           element={<HostVanPricing />}
+//           loader={async ({ request }) => await requireAuth(request)}
+//         />
+//         <Route
+//           path="photos"
+//           element={<HostVanPhotos />}
+//           loader={async ({ request }) => await requireAuth(request)}
+//         />
+//       </Route>
+// ))
+
---
+++ src/style/pages/store/StoreLayout.tsx
@@ -0,0 +1,11 @@
+import Store from "./Store";
+import StoreAside from "./StoreAside";
+
+export default function StoreLayout(){
+    return(
+        <>
+            <StoreAside></StoreAside>
+            <Store></Store>
+        </>
+    )
+}
